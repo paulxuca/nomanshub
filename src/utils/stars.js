17b553d377b181f258b01511ad015cbab29800cc
Paul Xu <paulxuca@gmail.com>
2016-11-18 09:32:43 -0500

    stars

--- src/utils/stars.js
+++ src/utils/stars.js
@@ -1,26 +1,37 @@
 import { randomRange } from './space';
 const THREE = require('three');
 
+
+const createStar = (gem, mat, scale, WIDTH, HEIGHT) => {
+  const sphere = new THREE.Mesh(gem, mat);
+  sphere.position.x = randomRange(-WIDTH, WIDTH);
+  sphere.position.y = randomRange(-HEIGHT, HEIGHT);
+
+    // Then set the z position to where it is in the loop (distance of camera)
+  sphere.position.z = randomRange(-500, 500);
+
+    // scale it up a bit
+  sphere.scale.x = sphere.scale.y = scale;
+  return sphere;
+}
+
+export const generateFollowerStars = (scene, followers, WIDTH, HEIGHT) => {
+  const followerStars = [];
+
+}
+
+
 export const generateStars = (scene, numStars, WIDTH, HEIGHT) => {
   const stars = [];
+  
   let totalStars = numStars;
     while(totalStars > 0) {
       const starGeom = new THREE.SphereGeometry(0.5, 32, 32);
       const starMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-      const sphere = new THREE.Mesh(starGeom, starMaterial);
+      const newStar = createStar(starGeom, starMaterial, 2, WIDTH, HEIGHT);
 
-      sphere.position.x = randomRange(-WIDTH, WIDTH);
-      sphere.position.y = randomRange(-HEIGHT, HEIGHT);
-
-        // Then set the z position to where it is in the loop (distance of camera)
-      sphere.position.z = randomRange(-500, 500);
-
-        // scale it up a bit
-      sphere.scale.x = sphere.scale.y = 2;
-
-        //add the sphere to the scene
-      scene.add(sphere);
-      stars.push(sphere);
+      scene.add(newStar);
+      stars.push(newStar);
       totalStars -= 1;
     }
     return stars;
